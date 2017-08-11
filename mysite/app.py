f1ab2dc48a76716377d4dc923b67fdde5efd28ed
Peter Ye <yecongzhoupeter@qq.com>
2017-08-11 10:07:57 +0800

    add error.html

--- mysite/app.py
+++ mysite/app.py
@@ -1,17 +1,28 @@
 from flask import render_template
 from flask import Flask
 from flask import request
-from flask_wtf import Form
+from flask_wtf import FlaskForm
 from wtforms import StringField,SubmitField
 from wtforms.validators import Required
 from flask_bootstrap import Bootstrap
 from form import DataForm,NameForm
+import thread
+import time
 
 app = Flask(__name__)
 app.config['SECRET_KEY']='hard to guess string'
 bootstrap = Bootstrap(app)
 
- 
+def isVaildDate(date):
+    try:
+       gettime = time.strptime(date, "%Y-%m-%d")
+       return True
+    except:
+       return False
+
+def doanything(year,month,day):
+    return True
+
 @app.route('/',methods=['GET','POST'])
 def index():
     Year = None
@@ -23,6 +34,11 @@ def index():
        Year = form.Year.data
        Month = form.Month.data
        Day = form.Day.data
+       date = Year+Month+Day
+       if isVaildDate(date) == True:
+           thread.start_new_thread(doanything,(Year,Month,Day))
+       else:
+           return render_template('error.html')
        form.Year.data = ''
        form.Month.data = ''
        form.Day.data = ''
