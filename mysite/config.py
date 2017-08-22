9540ee31b8836c85bad069f94c87625dc011285e
Peter Ye <yecongzhoupeter@qq.com>
2017-08-22 16:20:32 +0800

    add all

---
+++ mysite/config.py
@@ -0,0 +1,3 @@
+DEBUG = True # Turns on debugging features in Flask
+BCRYPT_LEVEL = 12 # Configuration for the Flask-Bcrypt extension
+MAIL_FROM_EMAIL = "704454136@qq.com" # For use in application emails
