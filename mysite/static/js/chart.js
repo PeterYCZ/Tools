038dd8fa4aae013961ce60fd39a79f0df15e7161
Peter Ye <yecongzhoupeter@qq.com>
2017-08-24 15:16:55 +0800

    try to add function but failed

--- mysite/static/js/chart.js
+++ mysite/static/js/chart.js
@@ -1,14 +1,21 @@
-var sqldata = [1,2,3,4,5]
+//var sqldata = [14,13,12,11,10,9,8,7,6,5,4,3,2,1]
+var sqldata = null;
+
+function getdata(data){
+  sqldata = data;
+};
+
+
 var data = {
-  labels: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri'],
+  labels: ['1', '2', '3', '4', '6','7', '8', '9', '10', '11','12','13','14'],
   series: [
     sqldata,
   ]
 };
 
 var options = {
-  width: 300,
-  height: 200
+  width: 600,
+  height: 600
 };
 
 new Chartist.Line('.ct-chart', data, options);
