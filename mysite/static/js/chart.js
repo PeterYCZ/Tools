8660090736f4da9f74277e477faaaedd85198181
Peter Ye <yecongzhoupeter@qq.com>
2017-09-15 10:00:29 +0800

    new tag Download and Chart

--- mysite/static/js/chart.js
+++ mysite/static/js/chart.js
@@ -1,6 +1,11 @@
 //var sqldata = [14,13,12,11,10,9,8,7,6,5,4,3,2,1]
 var sqldata = null;
 
+var elem=document.getElementById("chart1");
+  elem.style.position = "relative";
+  elem.style.top = "90px"
+  elem.style.left = "280px";
+
 function getdata(olddata,newdata){
   sqlolddata = olddata;
   sqlnewdata = newdata
@@ -18,7 +23,12 @@ function getdata(olddata,newdata){
 
   var options = {
     width: 600,
-    height: 600
+    height: 600,
+    chartPadding: { top: 0, right: 0, bottom: 0, left: 0},
+    classNames: {
+      point: 'ct-point ct-white',
+      line: 'ct-line ct-white'
+    }
   };
 
   new Chartist.Line('.ct-chart', data, options);
